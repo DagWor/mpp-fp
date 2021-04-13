1c0be54adad5f63125954350f2ac8d615820e308
dagwor <dagmawiworku11@gmail.com>
2021-04-13 16:53:14 -0500

    teller to string added and getters and setters

--- src/main/java/models/Teller.java
+++ src/main/java/models/Teller.java
@@ -8,6 +8,47 @@ public class Teller extends User {
         this.salary = salary;
     }
 
+
+    @Override
+    public String getEmail() {
+        return super.getEmail();
+    }
+
+    @Override
+    public String getFirstname() {
+        return super.getFirstname();
+    }
+
+    @Override
+    public String getLastName() {
+        return super.getLastName();
+    }
+
+    @Override
+    public String getUsername() {
+        return super.getUsername();
+    }
+
+    @Override
+    public void setEmail(String email) {
+        super.setEmail(email);
+    }
+
+    @Override
+    public void setFirstname(String firstname) {
+        super.setFirstname(firstname);
+    }
+
+    @Override
+    public void setUsername(String username) {
+        super.setUsername(username);
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        super.setLastName(lastName);
+    }
+
     public float getSalary() {
         return salary;
     }
@@ -20,5 +61,14 @@ public class Teller extends User {
     public void createCheckingAccount(Customer customer, SavingAccount savingAccount){
         // TODO - add account for customer
 //        customer.addAccount(checkingAccount);
+    }
+
+    @Override
+    public String toString() {
+        return "Teller {" +
+                "first name =" + getFirstname() +
+                "last name =" + getLastName() +
+                "email =" + getEmail() +
+                '}';
     }
 }
