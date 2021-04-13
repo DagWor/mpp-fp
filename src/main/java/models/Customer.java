5d5c065baf5c1b6e5dc0b9646416e1d6b2d78e66
mintuta <mintefikr4@gmail.com>
2021-04-13 11:35:08 -0500

    view Balance and fund transfer methods

--- src/main/java/models/Customer.java
+++ src/main/java/models/Customer.java
@@ -2,8 +2,16 @@ package models;
 
 public class Customer extends User {
 
-
     public Customer(String username, String firstname, String lastName, String email) {
         super(username, firstname, lastName, email);
+    }
+
+    public double viewBalance(){
+
+        return  0.0;
+    }
+
+    public  void fundTransfer(){
+
     }
 }
