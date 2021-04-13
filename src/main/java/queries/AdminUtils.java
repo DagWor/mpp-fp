92085ad836671109559bb70bad1b26e2bc8a91a3
dagwor <dagmawiworku11@gmail.com>
2021-04-13 16:46:43 -0500

    one function for admin

--- src/main/java/queries/AdminUtils.java
+++ src/main/java/queries/AdminUtils.java
@@ -9,14 +9,16 @@ import java.util.function.Function;
 import java.util.stream.Stream;
 
 public abstract class AdminUtils {
-    public static Function<HQ, Account> accountWithMostTransactions = hq ->
+    public static Function<HQ, Customer> mostActiveCustomer = hq ->
             Stream.of(hq)
                     .flatMap(hq1 -> hq1.getBranch().stream())
                     .flatMap(branch -> branch.getUsers().stream())
                     .filter(user -> user instanceof Customer)
                     .map(user -> (Customer) user)
                     .flatMap(customer -> customer.getAccountList().stream())
-                    .min((account1, account2) -> account2.getTransactionList().size() - account1.getTransactionList().size())
-                    .orElse(null);
+                    .sorted((account1, account2) -> account2.getTransactionList().size() - account1.getTransactionList().size())
+                    .limit(1)
+                    .map(Account::getCustomer)
+                    .findFirst().orElse(null);
 
 }
