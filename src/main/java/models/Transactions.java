fc220d12c8f2dc91d18f75f732a16ef67749b105
Eden Tesfahun <asnakeeden@gmail.com>
2021-04-13 11:49:34 -0500

    Transactions  skeleton class added

---
+++ src/main/java/models/Transactions.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.time.LocalDate;
+
+public class Transactions {
+
+    private  int transactionId;
+    private  String transactionType;
+    private  int fromAccount;
+    private int toAccount;
+    private double amount;
+    private LocalDate date;
+
+
+
+}
