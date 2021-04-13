4c4d1b848577a9a093b470cd9bc77f6b60a2c8fa
dagwor <dagmawiworku11@gmail.com>
2021-04-13 16:46:12 -0500

    admin fixed

--- src/main/java/models/Admin.java
+++ src/main/java/models/Admin.java
@@ -3,8 +3,8 @@ package models;
 public class Admin extends User {
     private final float salary;
 
-    public Admin(float salary, String username, String email, String firstName, String lastName) {
-        super(username, email, firstName, lastName);
+    public Admin(float salary, String username, String email, String firstName, String lastName, Address address) {
+        super(username, email, firstName, lastName, address);
         this.salary = salary;
     }
 
