bb8fbb4957accc8998c797a2dc4b872bd8def59c
dagwor <dagmawiworku11@gmail.com>
2021-04-14 11:31:00 -0500

    additional test cases

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MainTest {
 
-    public HQ prepareData(){
+    public List<Object> prepareData(){
 
         Address address = new Address("1000", "Fairfield", 52556, 10231, "IA", "USA");
         HQ hq = new HQ(
@@ -23,24 +23,23 @@ public class MainTest {
         Branch branch = new Branch("First", new Date(), address);
 
         User admin = new Admin(1000, "admin", "xyz@gmail.com", "John", "Doe", address);
-        User teller = new Teller(1000, "teller", "teller@gmail.com", "Alice", "Doe", address);
+        Teller teller = new Teller(1000, "teller", "teller@gmail.com", "Alice", "Doe", address);
         Customer customer = new Customer("customer", "customer@gmail.com", "Susan", "Doe", address);
 
         branch.setUsers(Arrays.asList(admin, customer, teller));
 
         Account account = new SavingAccount(123, 100, 5.5);
-        Transactions transactions = new Transactions("transfer", 123, 321, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions = new Transactions("withdrawal", 123, 321, 160.0, LocalDate.of(2021, 5, 2));
         Transactions transactions1 = new Transactions("deposit", 0, 123, 30.0, LocalDate.of(2021, 5, 2));
 
         Account account1 = new CheckingAccount(321, 2541.2, 7.5);
-        Customer customer2 = (Customer) customer;
-        account.setCustomer(customer2);
-        account1.setCustomer(customer2);
-        customer2.setAccountList(Arrays.asList(account, account1));
+        account.setCustomer(customer);
+        account1.setCustomer(customer);
+        customer.setAccountList(Arrays.asList(account, account1));
 
 
         account.setTransactionList(Arrays.asList(transactions, transactions1));
-        account1.setTransactionList(Arrays.asList(transactions));
+        account1.setTransactionList(Collections.singletonList(transactions));
 
         // Branch 2
 
@@ -48,7 +47,7 @@ public class MainTest {
 
         User admin1 = new Admin(1000, "admin1", "xyz1@gmail.com", "Dave", "George", address);
         Customer customer1 = new Customer("customer1", "Lily", "Hugh", "customer1@gmail.com", address);
-        Teller teller1 = new Teller(1000, "teller1", "teller1@gmail.com", "Carl", "Junior", address);
+        Teller teller1 = new Teller(1100, "teller1", "teller1@gmail.com", "Carl", "Junior", address);
 
         branch1.setUsers(Arrays.asList(admin1, customer1, teller1));
 
@@ -59,22 +58,21 @@ public class MainTest {
         Transactions transactions3 = new Transactions("deposit", 0, 123, 30.0, LocalDate.of(2021, 5, 2));
 
         Account account3 = new CheckingAccount(321, 2541.2, 7.5);
-//        Customer customer3 = (Customer) customer1;
         account3.setCustomer(customer1);
         account2.setCustomer(customer1);
         customer1.setAccountList(Arrays.asList(account3, account2));
 
 
         account2.setTransactionList(Arrays.asList(transactions2, transactions3));
-        account3.setTransactionList(Arrays.asList(transactions3));
+        account3.setTransactionList(Collections.singletonList(transactions3));
 
 
         hq.setBranch(Arrays.asList(branch, branch1));
 
-        return hq;
+        return Arrays.asList(hq, branch);
     }
 
-    public Customer expectedCustomer(){
+    public Customer expectedCustomer_whenHQGiven_provideMostActiveCustomer(){
         Address address = new Address("1000", "Fairfield", 52556, 10231, "IA", "USA");
         Customer customer = new Customer("customer", "customer@gmail.com", "Susan", "Doe", address);
 
@@ -89,17 +87,59 @@ public class MainTest {
 
 
         account.setTransactionList(Arrays.asList(transactions, transactions1));
-        account1.setTransactionList(Arrays.asList(transactions));
+        account1.setTransactionList(Collections.singletonList(transactions));
 
         return customer;
     }
 
+    public Teller expectedTeller_whenBranchGiven_provideHighestPaidTeller(){
+        Address address = new Address("1000", "Fairfield", 52556, 10231, "IA", "USA");
+        Teller teller = new Teller(1000, "teller", "teller@gmail.com", "Alice", "Doe", address);;
+
+        return teller;
+    }
+
+    public Double expectedRevenue_whenBranchGiven_calculateBranchRevenue(){
+        return -290.0D;
+    }
+
+    // TEST - mostActiveCustomer
     @Test
     public void whenHQGiven_provideMostActiveCustomer() {
 
+        List<Customer> expected = Collections.singletonList(expectedCustomer_whenHQGiven_provideMostActiveCustomer());
+        List<Customer> actual = AdminUtils.mostActiveCustomers.apply((HQ)prepareData().get(0), 1);
+        assertEquals(expected, actual);
+    }
 
-        List<Customer> expected = Arrays.asList(expectedCustomer());
-        List<Customer> actual = AdminUtils.mostActiveCustomers.apply(prepareData(), 1);
+    @Test
+    public void whenNullHQGiven_provideMostActiveCustomer() {
+
+        List<Customer> expected =new ArrayList<>();
+        List<Customer> actual = AdminUtils.mostActiveCustomers.apply(null, 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenHQGivenWithNullBranch_provideMostActiveCustomer() {
+
+        List<Customer> expected =new ArrayList<>();
+        List<Customer> actual = AdminUtils.mostActiveCustomers.apply(null, 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenBranchGiven_provideHighestPaidTeller() {
+
+        List<Teller> expected = Collections.singletonList(expectedTeller_whenBranchGiven_provideHighestPaidTeller());
+        List<Teller> actual = AdminUtils.highestPaidTellers.apply((Branch) prepareData().get(1), 1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void whenBranchGiven_calculateBranchRevenue(){
+        Double expected = expectedRevenue_whenBranchGiven_calculateBranchRevenue();
+        Double actual = AdminUtils.branchRevenue.apply((Branch) prepareData().get(1));
         assertEquals(expected, actual);
     }
 }
