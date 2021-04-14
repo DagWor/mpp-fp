bad98afdbe193edeb78edd5a975fffa9b077b64e
dagwor <dagmawiworku11@gmail.com>
2021-04-13 22:10:41 -0500

    admin utils refined

--- src/main/java/utility/AdminUtils.java
+++ src/main/java/utility/AdminUtils.java
@@ -4,20 +4,25 @@ import models.Account;
 import models.Customer;
 import models.HQ;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.*;
 
 public abstract class AdminUtils {
-    public static Function<HQ, Customer> mostActiveCustomer = hq ->
-            Stream.of(hq)
-                    .flatMap(hq1 -> hq1.getBranch().stream())
-                    .flatMap(branch -> branch.getUsers().stream())
+    public static BiFunction<HQ, Integer, List<Customer>> mostActiveCustomers = (hq, k) ->
+            Stream.ofNullable(hq)
+                    .flatMap(hq1 -> Optional.ofNullable(hq1.getBranch()).get().stream())
+                    .flatMap(branch -> Optional.ofNullable(branch.getUsers()).get().stream())
                     .filter(user -> user instanceof Customer)
                     .map(user -> (Customer) user)
-                    .flatMap(customer -> customer.getAccountList().stream())
+                    .flatMap(customer -> Optional.ofNullable(customer.getAccountList()).get().stream())
                     .sorted((account1, account2) -> account2.getTransactionList().size() - account1.getTransactionList().size())
-                    .limit(1)
+                    .limit(k)
                     .map(Account::getCustomer)
-                    .findFirst().orElse(null);
+                    .collect(Collectors.toList());
+
+//    public abstract
 
 }
