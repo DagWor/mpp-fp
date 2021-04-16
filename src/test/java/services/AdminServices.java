d7b006e6db96c9f4763c38be524ca8e523f57ee0
dagwor <dagmawiworku11@gmail.com>
2021-04-15 22:47:41 -0500

    new data population

--- src/test/java/services/AdminServices.java
+++ src/test/java/services/AdminServices.java
@@ -46,59 +46,84 @@ public class AdminServices {
 
         User admin1 = new Admin(1000, "admin1", "xyz1@gmail.com", "Dave", "George", address);
         Customer customer1 = new Customer("customer1", "Lily", "Hugh", "customer1@gmail.com", address);
+        Customer customer2 = new Customer("customer2", "John", "Tom", "customer2@gmail.com", address);
+        Customer customer3 = new Customer("customer3", "Dagi", "Pierre", "customer3@gmail.com", address);
+        Customer customer4 = new Customer("customer4", "Wende", "ed", "customer4@gmail.com", address);
         Teller teller1 = new Teller(1100, "teller1", "teller1@gmail.com", "Carl", "Junior", address);
-
-        branch1.setUsers(Arrays.asList(admin1, customer1, teller1));
 
 
 
         Account account2 = new SavingAccount(1234, 1500, 5.5);
-        Transactions transactions2 = new Transactions("transfer", 1234, 321, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions2 = new Transactions("withdrawal", 1234, 321, 150.0, LocalDate.of(2021, 5, 2));
         Transactions transactions3 = new Transactions("deposit", 0, 123, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions4 = new Transactions("withdrawal", 1234, 321, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions5 = new Transactions("deposit", 0, 123, 150.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions6 = new Transactions("withdrawal", 1234, 321, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions7 = new Transactions("deposit", 0, 123, 320.0, LocalDate.of(2021, 5, 2));
 
         Account account3 = new CheckingAccount(321, 2541.2, 7.5);
+        Account account4 = new CheckingAccount(321, 2541.2, 7.5);
+        Account account5 = new CheckingAccount(321, 2541.2, 7.5);
+        Account account6 = new CheckingAccount(321, 2541.2, 7.5);
+        Account account7 = new CheckingAccount(321, 2541.2, 7.5);
+
+
+        account7.setCustomer(customer4);
+        account6.setCustomer(customer4);
+        account5.setCustomer(customer3);
+        account4.setCustomer(customer2);
         account3.setCustomer(customer1);
         account2.setCustomer(customer1);
         customer1.setAccountList(Arrays.asList(account3, account2));
 
 
-        account2.setTransactionList(Arrays.asList(transactions2, transactions3));
+        account2.setTransactionList(Arrays.asList(transactions2, transactions3, transactions4, transactions5, transactions6, transactions7));
         account3.setTransactionList(Collections.singletonList(transactions3));
 
 
+
+        branch1.setUsers(Arrays.asList(admin1, customer1, customer2, customer3, customer4, teller1));
         hq.setBranch(Arrays.asList(branch, branch1));
 
-        return Arrays.asList(hq, branch);
+        return Arrays.asList(hq, branch1);
     }
 
     public static Customer expectedCustomer_whenHQGiven_provideMostActiveCustomer(){
         Address address = new Address("1000", "Fairfield", 52556, 10231, "IA", "USA");
-        Customer customer = new Customer("customer", "customer@gmail.com", "Susan", "Doe", address);
+
+        Customer customer = new Customer("customer1", "Lily", "Hugh", "customer1@gmail.com", address);
 
         Account account = new SavingAccount(123, 100, 5.5);
         Transactions transactions = new Transactions("transfer", 123, 321, 30.0, LocalDate.of(2021, 5, 2));
         Transactions transactions1 = new Transactions("deposit", 0, 123, 30.0, LocalDate.of(2021, 5, 2));
 
-        Account account1 = new CheckingAccount(321, 2541.2, 7.5);
-        account.setCustomer(customer);
-        account1.setCustomer(customer);
-        customer.setAccountList(Arrays.asList(account, account1));
+        Account account3 = new CheckingAccount(321, 2541.2, 7.5);
+        Account account2 = new SavingAccount(1234, 1500, 5.5);
+        account3.setCustomer(customer);
+        account2.setCustomer(customer);
+        customer.setAccountList(Arrays.asList(account3, account2));
+
+        Transactions transactions2 = new Transactions("withdrawal", 1234, 321, 150.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions3 = new Transactions("deposit", 0, 123, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions4 = new Transactions("withdrawal", 1234, 321, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions5 = new Transactions("deposit", 0, 123, 150.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions6 = new Transactions("withdrawal", 1234, 321, 30.0, LocalDate.of(2021, 5, 2));
+        Transactions transactions7 = new Transactions("deposit", 0, 123, 320.0, LocalDate.of(2021, 5, 2));
 
 
-        account.setTransactionList(Arrays.asList(transactions, transactions1));
-        account1.setTransactionList(Collections.singletonList(transactions));
+        account2.setTransactionList(Arrays.asList(transactions2, transactions3, transactions4, transactions5, transactions6, transactions7));
+        account.setTransactionList(Collections.singletonList(transactions));
 
         return customer;
     }
 
     public static Teller expectedTeller_whenBranchGiven_provideHighestPaidTeller(){
         Address address = new Address("1000", "Fairfield", 52556, 10231, "IA", "USA");
-        Teller teller = new Teller(1000, "teller", "teller@gmail.com", "Alice", "Doe", address);;
 
-        return teller;
+        return new Teller(1100, "teller1", "teller1@gmail.com", "Carl", "Junior", address);
     }
 
     public static Double expectedRevenue_whenBranchGiven_calculateBranchRevenue(){
-        return 290.0D;
+        return 320.0D;
     }
 }
